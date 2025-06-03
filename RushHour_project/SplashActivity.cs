65b1f115ad59a08509e0a528ea9f11f09e37c88b
samromanov <samromanov2007@gmail.com>
2025-06-03 15:24:59 +0300

    Update SplashActivity.cs

--- RushHour_project/SplashActivity.cs
+++ RushHour_project/SplashActivity.cs
@@ -14,7 +14,7 @@ using AndroidX.AppCompat.App;
 
 namespace RushHour_project
 {
-    [Activity(Label = "SplashActivity", Theme = "@style/AppTheme", MainLauncher = true, NoHistory = true)]
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true, NoHistory = true)]
     public class SplashActivity : Activity
     {
         protected override void OnCreate(Bundle savedInstanceState)
